ecec4022d92c690971acf1be8e14c49c57affcbc
Viktor Malik <viktor.malik@gmail.com>
2024-01-06 22:19:11 +0100

    Rename variables to lowercase

--- inkgen.sh
+++ inkgen.sh
@@ -1,33 +1,33 @@
 #! /bin/bash
 
-INPUT=$1
-OUTPUT=$2
+input=$1
+output=$2
 
-OBJ=${INPUT%.svg}
-OBJ_CNT=$(inkscape --actions "select-all;select-list" $INPUT 2>/dev/null | \
-          grep ^$OBJ-[0-9] | \
+obj=${input%.svg}
+obj_cnt=$(inkscape --actions "select-all;select-list" $input 2>/dev/null | \
+          grep ^$obj-[0-9] | \
           cut -d' ' -f1 | cut -d'-' -f2 | \
           sort -n | tail -n 1)
 
-ROWS=5
-COLS=4
+rows=5
+cols=4
 
-ACTIONS=
-for r in $(seq $ROWS); do
-    ids=$(seq $OBJ_CNT)
-    for c in $(seq $COLS); do
+actions=
+for r in $(seq $rows); do
+    ids=$(seq $obj_cnt)
+    for c in $(seq $cols); do
         id=$(shuf -e -n 1 $ids)
         ids=${ids/$id/}
-        ACTIONS+="select-by-id:$OBJ-$id;"
-        ACTIONS+="clone;"
-        ACTIONS+="select-by-id:$OBJ-anchor-$r-$c;"
-        ACTIONS+="object-align:hcenter last;"
-        ACTIONS+="object-align:vcenter last;"
-        ACTIONS+="select-clear;"
+        actions+="select-by-id:$obj-$id;"
+        actions+="clone;"
+        actions+="select-by-id:$obj-anchor-$r-$c;"
+        actions+="object-align:hcenter last;"
+        actions+="object-align:vcenter last;"
+        actions+="select-clear;"
     done
 done
 
-ACTIONS+="export-filename:$OUTPUT;"
-ACTIONS+="export-do;"
+actions+="export-filename:$output;"
+actions+="export-do;"
 
-inkscape --actions "$ACTIONS" $INPUT
+inkscape --actions "$actions" $input
